0cd1c4611bc88351e191dcda6d36e30793dd6eac
alinen <anormoyle@brynmawr.edu>
2025-09-10 14:45:27 -0400

    post A02

---
+++ A02/compute.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TYPE_SIZE 4
+
+int add(int a[TYPE_SIZE], int b[TYPE_SIZE], int c[TYPE_SIZE], int carryin)
+{
+  return 0;
+}
+
+int main(int argc, char** argv)
+{
+  if (argc != 4)
+  {
+    printf("usage: %s <num1> <op> <num2>\n", argv[0]);
+    return 0;
+  }
+  
+  return 0;
+}
+
